a4a9a18c9ee3e76ec14b26112b4f272cbae692fc
Edgar Vallejo <bam_vllj98@outlook.com>
2020-03-04 15:00:11 -0600

    Versión funcional del programa :)

--- source/CAFETERA FAST LINE.c
+++ source/CAFETERA FAST LINE.c
@@ -15,6 +15,7 @@ void ADC0_IRQHandler(void);
 
 int menu;
 int banderaADC = 0, temperaturaADC = 0, posicionTaza = 0;
+int aux1, aux2;
 
 int main(void) {
 
@@ -30,7 +31,7 @@ int main(void) {
     timer(1,1);
 
     while(true) {
-    	printf("\r1.- Programa principal 2.-Condiciones 3.- calentar el agua 4.- echar agua\n\r");
+    	printf("\r1.- Programa principal 2.-Pruebas:\n\r");
     	scanf("%d", &menu);
 
     	if(menu == 1){
@@ -39,9 +40,55 @@ int main(void) {
     				"\rPosicion: %d\n", temperaturaADC, posicionTaza);
     		//Ingrese proceso principal
     	}
-    	if(menu == 2)	condicionesParaPrepararCafe(&posicionTaza, &temperaturaADC, &banderaADC);
-    	if(menu == 3)	calentarAgua(&banderaADC, &temperaturaADC);
-    	if(menu == 4)  	prenderTermoTaza;
+    	if(menu == 2)
+    		while(menu){
+    			printf("1.-Moler café	2.-mover Rodillo	3.-Mover Taza\n\r"
+    					"4.-Taza a tope	5.-Llenar Taza	6.-Enjuague\n\r"
+    					"7.- Leer temp	8.-Condiciones\n\r"
+    					"0.- Salir\n\r");
+    			scanf("%d", &menu);
+
+    			switch(menu){
+    			case 1:
+    				molerCafe(8, 3);
+    				break;
+
+    			case 2:
+    				moverRodillo();
+    				break;
+
+    			case 3:
+    				printf("Ingrese el número de pasos: ");
+    				scanf("%d", &aux1);
+    				printf("\n\r0 para subir, 1 para bajar: ");
+    				scanf("%d", &aux2);
+    				moverTaza(aux1, aux2, &posicionTaza);
+    				break;
+
+    			case 4:
+    				printf("\n\r0 para subir, 1 para bajar: ");
+    				scanf("%d", &aux1);
+    				tazaATope(aux1, &posicionTaza);
+    				break;
+
+    			case 5:
+    				llenarTaza(7);
+    				break;
+
+    			case 6:
+    				enjuague(1);
+    				break;
+
+    			case 7:
+    				leerTemperatura(&banderaADC, &temperaturaADC);
+    				printf("\n\rtemperatura: %d\n\r", temperaturaADC);
+    				break;
+
+    			case 8:
+    				condicionesParaPrepararCafe(&posicionTaza, &temperaturaADC, &banderaADC);
+    				break;
+    			}
+    		}
     }
 
     return 0;
